5ebce22eac5bf45681030290686eebcf8528469a
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-12-03 20:08:37 -0700

    create project

---
+++ C-Sharp-Object-Oriented-Design-Final-Project/C-Sharp-Object-Oriented-Design-Final-Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_Sharp_Object_Oriented_Design_Final_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
