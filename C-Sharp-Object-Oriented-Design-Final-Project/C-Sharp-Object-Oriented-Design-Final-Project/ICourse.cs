c6c9eed5e80b5e169f82dd28b2f2a432ddbedac3
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-12-03 23:34:00 -0700

    add interfaces

---
+++ C-Sharp-Object-Oriented-Design-Final-Project/C-Sharp-Object-Oriented-Design-Final-Project/ICourse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace C_Sharp_Object_Oriented_Design_Final_Project
+{
+    internal interface ICourse // Interfaces
+    {
+        public string GetName();
+
+        public int GetId();
+
+        public List<Student> GetStudents();
+
+        public void AddStudent(Student newStudent);
+
+        public void RemoveStudent(Student student);
+    }
+}
---
+++ C-Sharp-Object-Oriented-Design-Final-Project/C-Sharp-Object-Oriented-Design-Final-Project/IStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Object_Oriented_Design_Final_Project
+{
+    internal interface IStudent // Interfaces
+    {
+        public void AddCourse(Course course);
+
+        public void DropCourse(Course course);
+    }
+}
