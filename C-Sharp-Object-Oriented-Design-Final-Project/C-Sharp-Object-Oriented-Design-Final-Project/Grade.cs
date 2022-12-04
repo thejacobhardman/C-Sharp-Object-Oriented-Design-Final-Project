1672990f41814e3b822c50b6f88880f717513c58
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-12-03 23:34:52 -0700

    add grade class

---
+++ C-Sharp-Object-Oriented-Design-Final-Project/C-Sharp-Object-Oriented-Design-Final-Project/Grade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Object_Oriented_Design_Final_Project
+{
+    internal class Grade
+    {
+        public Grade()
+        {
+            Value = 0.0;
+            Course = new Course();
+        }
+
+        public Grade(double value)
+        {
+            Value = value;
+            Course = new Course();
+        }
+
+        public Grade (Course course)
+        {
+            Value = 0.0;
+            Course = course;
+        }
+
+        public Grade (double value, Course course)
+        {
+            Value = value;
+            Course = course;
+        }
+
+        protected double Value; // Encapsulation
+
+        protected Course Course; // Encapsulation
+
+        public double GetGrade() { return Value; } // Encapsulation
+
+        public Course GetCourse() { return Course; } // Encapsulation
+    }
+}
