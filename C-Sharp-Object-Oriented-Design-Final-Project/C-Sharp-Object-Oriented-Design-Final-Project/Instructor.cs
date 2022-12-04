6363853750f3c4a5622893c1f219233b2d43d933
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-12-03 23:34:31 -0700

    add instructor class

---
+++ C-Sharp-Object-Oriented-Design-Final-Project/C-Sharp-Object-Oriented-Design-Final-Project/Instructor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Object_Oriented_Design_Final_Project
+{
+    internal class Instructor : User // Inheritance
+    {
+        public Instructor(string firstName, string lastName, string role): base(firstName, lastName, role)
+        {
+            FirstName = firstName; // Inheritance
+            LastName = lastName; // Inheritance
+            Role = role; // Inheritance
+            Courses = new List<Course>();
+        }
+
+        protected List<Course> Courses; // Encapsulation
+
+        public List<Course> GetCourses() { return Courses; } // Encapsulation
+
+        public void AssignToCourse(Course course)
+        {
+            course.AssignInstructor(this);
+            Courses.Add(course);
+        }
+
+        public void DropCourse(Course course)
+        {
+            course.RemoveInstructor();
+            Courses.Remove(course);
+        }
+    }
+}
